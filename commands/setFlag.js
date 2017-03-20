efa6d2dd64f9b3fda70d846077304b187a072020
Funnbot <dillonbayless1@gmail.com>
2017-03-20 07:34:33 -0700

    Update setFlag.js

--- commands/setFlag.js
+++ commands/setFlag.js
@@ -1,6 +1,9 @@
 exports.run = (message, bot, send) => {
   if (!message.args[0]) return send("**Proudly display a flag on your profile**");
-  if (message.suffix.length > 9) return send("**flag emojis are only 9 characters long :flag_xx:**");
+  let flag = message.args[0]
+  if (flag > 9) return send("**flag emojis are only 9 characters long :flag_xx:**");
+  if (!/^:flag_\w\w:$/.test(flag) return send("no");
+  //We need to build a folder containing every single flag image
   bot.profile.setFlag(message.author.id, message.suffix);
   send("**Your flag was set to: **\n" + message.suffix)
 }
